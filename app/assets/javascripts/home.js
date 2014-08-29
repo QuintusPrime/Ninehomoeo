d86e2d45e54607969292b01a143c4927825f697c
Ketan Gupta <ketan936@gmail.com>
2014-08-29 17:58:34 +0530

    zoom master

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,19 +1,4 @@
-var current_h = null;
-var current_w = null;
+
 $(document).ready(function() {
-	$('.img-responsive').hover(
-	function() {
-		console.log("hovered");
-		current_h = $(this, 'img')[0].height;
-		current_w = $(this, 'img')[0].width;
-		$(this).animate({
-			width : (current_w * 2),
-			height : (current_h * 2)
-		}, 300);
-	}, function() {
-		$(this).animate({
-			width : current_w + 'px',
-			height : current_h + 'px'
-		}, 300);
-	});
+	$('.photo').zoom();
 });
