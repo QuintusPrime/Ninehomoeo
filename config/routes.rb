a147a845b6e07f7098ed14a8fd0f6ca55643cd70
Ketan Gupta <ketan936@gmail.com>
2014-09-04 16:32:54 +0530

    price range fixed

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get "/home/shop" => "home#shop"
   get "/home/login" => "home#login"
   get "/home/cart" => "home#cart"
+  get "/home/checkout" => "home#checkout"
 end
